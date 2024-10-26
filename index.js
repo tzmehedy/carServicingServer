bd8aeb76295b3d0facb4c60e958ff80eaf13124c
unknown <mehedytouhidurzaman@gmail.com>
2024-10-26 22:48:29 +0600

    connect to mongo db is completed

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+
 
 const app = express();
 
@@ -7,6 +11,41 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Car servicing data is coming");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8aq9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+     client.connect();
+    // Send a ping to confirm a successful connection
+     client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log("The port is running 5000");
