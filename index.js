93d3aee6317770178661d7fab93385da7f3235c7
unknown <mehedytouhidurzaman@gmail.com>
2024-10-26 23:50:33 +0600

    services data get from database is completed

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
-
-
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -23,29 +21,55 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    client.connect();
+
+
+    const serviceCollections = client.db("carServicing").collection("servicingCollections");
+
+
+    app.get("/services", async(req,res)=>{
+        const result = await serviceCollections.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
-     client.db("admin").command({ ping: 1 });
+
+    client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    
+    
+
+
+
+    
+
+    
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log("The port is running 5000");
