34a4ad2fc6b921fe3112710b1e4b2dfc85f076b1
unknown <mehedytouhidurzaman@gmail.com>
2024-10-28 03:08:27 +0600

    service data get for checkout page from mongo db in server is completed

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+
+const cors = require("cors")
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Car servicing data is coming");
@@ -37,14 +43,13 @@ async function run() {
         res.send(result)
     })
 
+    app.get("/services/:id", async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollections.findOne(query)
+      res.send(result)
 
-
-
-
-
-
-
-
+    })
 
     // Send a ping to confirm a successful connection
 
